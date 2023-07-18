c49c14109ef5f964f3dbe6988347133f2c194dde
Luke Marsden <me@lukemarsden.net>
2023-07-18 10:26:01 +0100

    WIP on lora

--- src/python/modules/lora.py
+++ src/python/modules/lora.py
@@ -1,61 +1,119 @@
 import json
 
-def _sdxl(params: str):
+def _lora(params: str):
     params = json.loads(params)
     if not isinstance(params, dict):
         raise Exception(
             "Please set params to a dict like "+
-            "{'mode': 'train', 'seed': 42}, or "+
-            "{'mode': 'inference', 'prompt': 'an astronaut in the style of <s1><s2>', seed: 11}")
-    
-    # TODO: fill in the rest of this with the two modes...
-    return {
-        "APIVersion": "V1beta1",
-        "Metadata": {
-            "CreatedAt": "0001-01-01T00:00:00Z",
-            "Requester": {}
-        },
-        "Spec": {
-            "Deal": {
-                "Concurrency": 1
+            "{'mode': 'training', 'seed': 42, 'input_cid': 'Qm...'} "+
+            "(where input cid has images in), or "+
+            "{'mode': 'inference', 'prompt': 'an astronaut in the style of <s1><s2>', "+
+            "seed: 11, 'lora_cid': 'Qm...'} "+
+            "where lora_cid is the output cid of the above step")
+
+    # TODO: update following
+    mode = params.get("mode", "inference")
+    if mode == "inference":
+        return {
+            "APIVersion": "V1beta1",
+            "Metadata": {
+                "CreatedAt": "0001-01-01T00:00:00Z",
+                "Requester": {}
             },
-            "Docker": {
-                "Entrypoint": [
-                    "python3",
-                    "inference.py",
-                ],
-                "Image": "quay.io/lukemarsden/sdxl:v0.9-lilypad1",
-                "EnvironmentVariables": [
-                    f"PROMPT={params.get('prompt', 'question mark floating in space')}",
-                    f"RANDOM_SEED={params.get('seed', 42)}",
-                    f"OUTPUT_DIR=/outputs/",
-                    "HF_HUB_OFFLINE=1",
+            "Spec": {
+                "Deal": {
+                    "Concurrency": 1
+                },
+                "Docker": {
+                    "Entrypoint": [
+                        "python3",
+                        "inference.py",
+                    ],
+                    "Image": "quay.io/lukemarsden/sdxl:v0.9-lilypad1",
+                    "EnvironmentVariables": [
+                        f"PROMPT={params.get('prompt', 'question mark floating in space')}",
+                        f"RANDOM_SEED={params.get('seed', 42)}",
+                        f"OUTPUT_DIR=/outputs/",
+                        "HF_HUB_OFFLINE=1",
+                    ]
+                },
+                "Engine": "Docker",
+                "Language": {
+                    "JobContext": {}
+                },
+                "Network": {
+                    "Type": "None"
+                },
+                "PublisherSpec": {
+                    "Type": "Estuary"
+                },
+                "Resources": {
+                    "GPU": "1"
+                },
+                "Timeout": 1800,
+                "Verifier": "Noop",
+                "Wasm": {
+                    "EntryModule": {}
+                },
+                "outputs": [
+                    {
+                        "Name": "outputs",
+                        "StorageSource": "IPFS",
+                        "path": "/outputs"
+                    }
                 ]
-            },
-            "Engine": "Docker",
-            "Language": {
-                "JobContext": {}
-            },
-            "Network": {
-                "Type": "None"
-            },
-            "PublisherSpec": {
-                "Type": "Estuary"
-            },
-            "Resources": {
-                "GPU": "1"
-            },
-            "Timeout": 1800,
-            "Verifier": "Noop",
-            "Wasm": {
-                "EntryModule": {}
-            },
-            "outputs": [
-                {
-                    "Name": "outputs",
-                    "StorageSource": "IPFS",
-                    "path": "/outputs"
-                }
-            ]
+            }
         }
-    }
+    elif mode == "training":
+        return {
+            "APIVersion": "V1beta1",
+            "Metadata": {
+                "CreatedAt": "0001-01-01T00:00:00Z",
+                "Requester": {}
+            },
+            "Spec": {
+                "Deal": {
+                    "Concurrency": 1
+                },
+                "Docker": {
+                    "Entrypoint": [
+                        "python3",
+                        "inference.py",
+                    ],
+                    "Image": "quay.io/lukemarsden/sdxl:v0.9-lilypad1",
+                    "EnvironmentVariables": [
+                        f"PROMPT={params.get('prompt', 'question mark floating in space')}",
+                        f"RANDOM_SEED={params.get('seed', 42)}",
+                        f"OUTPUT_DIR=/outputs/",
+                        "HF_HUB_OFFLINE=1",
+                    ]
+                },
+                "Engine": "Docker",
+                "Language": {
+                    "JobContext": {}
+                },
+                "Network": {
+                    "Type": "None"
+                },
+                "PublisherSpec": {
+                    "Type": "Estuary"
+                },
+                "Resources": {
+                    "GPU": "1"
+                },
+                "Timeout": 1800,
+                "Verifier": "Noop",
+                "Wasm": {
+                    "EntryModule": {}
+                },
+                "outputs": [
+                    {
+                        "Name": "outputs",
+                        "StorageSource": "IPFS",
+                        "path": "/outputs"
+                    }
+                ]
+            }
+        }
+    else:
+        raise Exception(f"unknown mode {mode}")
