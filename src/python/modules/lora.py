81f70b5e4c9810dcea2086e3f43bb22c059b0447
Luke Marsden <me@lukemarsden.net>
2023-07-18 17:15:43 +0000

    unzip input images because web3.storage only supports single file cids

--- src/python/modules/lora.py
+++ src/python/modules/lora.py
@@ -11,7 +11,7 @@ def _lora_training(params: str):
     if params.startswith("{"):
         params = yaml.safe_load(params)
     else:
-        raise Exception("Please set params to yaml like {seed: 42, 'images_cid': 'Qm...'}")
+        raise Exception("Please set params to yaml like {seed: 42, 'images_cid': 'Qm...'} where images_cid contains an images.zip with training images in")
 
     seed = params["seed"]
     images_cid = params["images_cid"]
@@ -27,25 +27,11 @@ def _lora_training(params: str):
                 "Concurrency": 1
             },
             "Docker": {
+                "EnvironmentVariables": [
+                    f"RANDOM_SEED={seed}",
+                ],
                 "Entrypoint": [
-                    'lora_pti',
-                    f'--pretrained_model_name_or_path={MODEL_NAME}',
-                    '--instance_data_dir=/input', '--output_dir=/output',
-                    '--train_text_encoder', '--resolution=512',
-                    '--train_batch_size=1',
-                    '--gradient_accumulation_steps=4', '--scale_lr',
-                    '--learning_rate_unet=1e-4',
-                    '--learning_rate_text=1e-5', '--learning_rate_ti=5e-4',
-                    '--color_jitter', '--lr_scheduler="linear"',
-                    '--lr_warmup_steps=0',
-                    '--placeholder_tokens="<s1>|<s2>"',
-                    '--use_template="style"', '--save_steps=100',
-                    '--max_train_steps_ti=1000',
-                    '--max_train_steps_tuning=1000',
-                    '--perform_inversion=True', '--clip_ti_decay',
-                    '--weight_decay_ti=0.000', '--weight_decay_lora=0.001',
-                    '--continue_inversion', '--continue_inversion_lr=1e-4',
-                    '--device="cuda:0"', '--lora_rank=1'
+                    "bash", "-c", f'(cd /input && unzip images.zip && rm images.zip) && lora_pti --pretrained_model_name_or_path={MODEL_NAME} --instance_data_dir=/input --output_dir=/output --train_text_encoder --resolution=512 --train_batch_size=1 --gradient_accumulation_steps=4 --scale_lr --learning_rate_unet=1e-4 --learning_rate_text=1e-5 --learning_rate_ti=5e-4 --color_jitter --lr_scheduler="linear" --lr_warmup_steps=0 --placeholder_tokens="<s1>|<s2>" --use_template="style" --save_steps=100 --max_train_steps_ti=1000 --max_train_steps_tuning=1000 --perform_inversion=True --clip_ti_decay --weight_decay_ti=0.000 --weight_decay_lora=0.001 --continue_inversion --continue_inversion_lr=1e-4 --device="cuda:0" --lora_rank=1'
                 ],
                 "Image": IMAGE,
             },
@@ -139,7 +125,7 @@ def _lora_inference(params: str):
 
                         patch_pipe(
                             pipe,
-                            "/input/final_lora.safetensors",
+                            "/input/outputs/final_lora.safetensors",
                             patch_text=True,
                             patch_ti=True,
                             patch_unet=True,
