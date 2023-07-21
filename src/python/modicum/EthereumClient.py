15634526a37f5e727c258fd4cd97e8c65e0d9e7b
Luke Marsden <me@lukemarsden.net>
2023-07-21 17:06:33 +0100

    gah

--- src/python/modicum/EthereumClient.py
+++ src/python/modicum/EthereumClient.py
@@ -161,7 +161,7 @@ class EthereumClient:
         else:
             try:
                 self.logger.info(
-                    "recreating filter from latest event block {self._latest_event_block_number}",
+                    f"recreating filter from latest event block {self._latest_event_block_number}",
                 )
                 self.filter = self.w3.eth.filter({
                     "fromBlock": min(
@@ -169,8 +169,8 @@ class EthereumClient:
                     ),
                 })
             except Exception as e:
-                self.logger.info("recreating filter from latest event block failed with {e}, "+
-                                 "resetting to latest, probably dropping some events :(")
+                self.logger.info(f"recreating filter from latest event block failed with {e}, "+
+                                  "resetting to latest, probably dropping some events :(")
                 self.filter = self.w3.eth.filter({"fromBlock": self.w3.eth.block_number})
 
     def exit(self):
