9194b80c73b6b3ff6f5b67edbee8d17973169687
Luke Marsden <me@lukemarsden.net>
2023-08-03 14:35:53 +0100

    wip

--- src/python/modules/filecoin_data_prep.py
+++ src/python/modules/filecoin_data_prep.py
@@ -1,4 +1,6 @@
-def _filecoin_data_prep(params: dict):
+import yaml
+
+def _filecoin_data_prep(params: str):
     """
     params["s3_bucket", "s3_key", "s3_region"]
     e.g.
@@ -6,9 +8,37 @@ def _filecoin_data_prep(params: dict):
         "s3_key": "ABI-L1b-RadC/2000/001/12/OR_ABI-L1b-RadC-M3C01*",
         "s3_region": "us-east-1" (optional)
 
+    or, use ipfs input:
+        "ipfs_cid": "bafybeiah7ib5mhzlckolwlkwquzf772wl6jdbhtbuvnbuo5arq7pcs4ubm"
+
     # TODO: add s3_endpoint! (to support S3-compatible endpoints other than real S3)
     # TODO: https://github.com/bacalhau-project/MODICUM/issues/9
     """
+    if params.startswith("{"):
+        params = yaml.safe_load(params)
+    else:
+        raise Exception("Please set params to yaml like {seed: 42, 'images_cid': 'Qm...'} where images_cid contains an images.zip with training images in")
+
+    inputs = []
+    if "ipfs_cid" in params:
+        inputs.append(
+                {"CID": params["ipfs_cid"],
+                 "Name": f"ipfs://{params['ipfs_cid']}",
+                 "StorageSource": "IPFS",
+                 "path": "/input",
+                 })
+    elif "s3_bucket" in params:
+        inputs.append({
+            "Name": "s3-input",
+            "S3": {
+                "Bucket": params["s3_bucket"],
+                "Key": params["s3_key"],
+                "Region": params.get("s3_region", "us-east-1")
+            },
+            "StorageSource": "S3",
+            "path": "/input"
+        })
+
     return {
         "APIVersion": "V1beta1",
         "Metadata": {
@@ -21,7 +51,7 @@ def _filecoin_data_prep(params: dict):
             },
             "Docker": {
                 "Entrypoint": [
-                    "/usr/local/bin/data-prep", "fil-data-prep",
+                    "data-prep", "fil-data-prep",
                       "--size", "100000000000", "--metadata", "meta.csv",
                       "--output", "/output/test", "/input",
                 ],
@@ -52,17 +82,6 @@ def _filecoin_data_prep(params: dict):
                     "path": "/output"
                 }
             ],
-            "inputs": [
-                {
-                    "Name": "s3-input",
-                    "S3": {
-                        "Bucket": params["s3_bucket"],
-                        "Key": params["s3_key"],
-                        "Region": params.get("s3_region", "us-east-1")
-                    },
-                    "StorageSource": "S3",
-                    "path": "/input"
-                }
-            ]
+            "inputs": inputs,
         }
     }
