da076375f21eca8d48cf5cc73f9cbce3f6ab6032
Alison Haire <alihaire900@gmail.com>
2023-09-02 01:18:35 +1000

    Update ExampleClient.sol

--- src/js/contracts/ExampleClient.sol
+++ src/js/contracts/ExampleClient.sol
@@ -23,4 +23,51 @@ contract ExampleClient {
 
   event ReceivedJobResults(uint256 jobID, string cid);
 
+  // The Modicum contract address is found here: https://github.com/bacalhau-project/lilypad-modicum/blob/main/latest.txt
+  // Current: 0x422F325AA109A3038BDCb7B03Dd0331A4aC2cD1a
+  constructor (address contractAddress) {
+    require(contractAddress != address(0), "NaiveExamplesClient: contract cannot be zero address");
+    _contractAddress = contractAddress;
+    remoteContractInstance = ModicumContract(contractAddress);
+  }
+
+  function setLilypadFee(uint256 _fee) public {
+      lilypadFee = _fee;
+  }
   
+  function runCowsay(string memory sayWhat) public payable returns (uint256) {
+    require(msg.value == lilypadFee * 1 ether, string(abi.encodePacked("Payment of 2 Ether is required")));
+    return runModule("cowsay:v0.0.1", sayWhat);
+  }
+
+  function runStablediffusion(string memory prompt) public payable returns (uint256) {
+    require(msg.value == lilypadFee * 1 ether, string(abi.encodePacked("Payment of 2 Ether is required")));
+    return runModule("stable_diffusion:v0.0.1", prompt);
+  }
+
+  function runSDXL(string memory prompt) public payable returns (uint256) {
+    require(msg.value == lilypadFee * 1 ether, string(abi.encodePacked("Payment of 2 Ether is required")));
+    return runModule("sdxl:v0.9-lilypad1", prompt);
+  }
+
+ function runModule(string memory name, string memory params) public payable returns (uint256) {
+    require(msg.value == lilypadFee * 1 ether, string(abi.encodePacked("Payment of 2 Ether is required")));
+    return remoteContractInstance.runModuleWithDefaultMediators{value: msg.value}(name, params);
+  }
+
+  // Implemented Modicum interface. This saves results to a results array
+  function receiveJobResults(uint256 _jobID, string calldata _cid) public {
+    Result memory jobResult = Result({
+        jobID: _jobID,
+        cid: _cid,
+        httpString: string(abi.encodePacked("https://ipfs.io/ipfs/", _cid))
+    });
+    results.push(jobResult);
+
+    emit ReceivedJobResults(_jobID, _cid);
+  }
+
+  function fetchAllResults() public view returns (Result[] memory) {
+    return results;
+  }
+}
